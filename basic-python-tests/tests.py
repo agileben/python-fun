344c35947fe1def2e357e14cf9b8dc2d383f7172
Ben Hogan <Ben@agileben.com>
2019-06-07 10:03:11 +0000

    Clean up tabs and style

--- basic-python-tests/tests.py
+++ basic-python-tests/tests.py
@@ -4,12 +4,12 @@ class MyThing:
     def add(self, num1, num2):
         return num1 + num2
 
+
 class MyTest(unittest.TestCase):
 
-  def test_pass(self):
-      pass
+    def test_pass(self):
+        pass
 
-  def test_add(self):
-      thing = MyThing()
-      self.assertEquals(thing.add(2, 2), 4)
-
+    def test_add(self):
+        thing = MyThing()
+        self.failUnlessEqual(thing.add(2, 2), 4)
